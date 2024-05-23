155722d6f132fdf0529b0b878e9b64b8bd064d47
MeZeksan <bulzak74848@gmail.com>
2024-05-23 18:42:06 +0500

    Добавил скролл RegistrationScreen и AutorizationScreen

--- lib/features/autorization_screen/screens/autorization_screen.dart
+++ lib/features/autorization_screen/screens/autorization_screen.dart
@@ -7,35 +7,37 @@ class AutorizationScreen extends StatelessWidget {
   @override
   Widget build(BuildContext context) {
     return Scaffold(
-      body: Padding(
-        padding: const EdgeInsets.fromLTRB(32, 24, 32, 0),
-        child: Center(
-          child: Column(
-            mainAxisAlignment: MainAxisAlignment.center,
-            children: [
-              Image.asset("assets/images/cafe.png"),
-              Text("Welcome back\nto",
-                  style: Theme.of(context).textTheme.headlineSmall,
-              textAlign: TextAlign.center,),
-              Text("OUR REMINDER",
-                  style: Theme.of(context).textTheme.headlineLarge),
-              const SizedBox(height:60),
-              const RoundTextField(hint: 'Enter your email'),
-              const SizedBox(height:20),
-              const RoundTextField(hint: 'Enter password'),
-              const SizedBox(height:60),
-              const ButtonText(text: 'Forgot your password?',nextScreen: RegistrationScreen()),
-              const SizedBox(height:60),
-              GetStartButton(text: 'Sign in'),
-              const SizedBox(height:20),
-              const Row(
-                mainAxisAlignment: MainAxisAlignment.center,
-                children: [
-                  Text("Don't have an account?"),
-                  ButtonText(text: 'Sing up',nextScreen: RegistrationScreen(),)
-                ],
-              )
-            ],
+      body: SingleChildScrollView(
+        child: Padding(
+          padding: const EdgeInsets.fromLTRB(32, 48, 32, 0),
+          child: Center(
+            child: Column(
+              mainAxisAlignment: MainAxisAlignment.center,
+              children: [
+                Image.asset("assets/images/cafe.png"),
+                Text("Welcome back\nto",
+                    style: Theme.of(context).textTheme.headlineSmall,
+                textAlign: TextAlign.center,),
+                Text("OUR REMINDER",
+                    style: Theme.of(context).textTheme.headlineLarge),
+                const SizedBox(height:60),
+                const RoundTextField(hint: 'Enter your email'),
+                const SizedBox(height:20),
+                const RoundTextField(hint: 'Enter password'),
+                const SizedBox(height:60),
+                const ButtonText(text: 'Forgot your password?',nextScreen: RegistrationScreen()),
+                const SizedBox(height:60),
+                GetStartButton(text: 'Sign in'),
+                const SizedBox(height:20),
+                const Row(
+                  mainAxisAlignment: MainAxisAlignment.center,
+                  children: [
+                    Text("Don't have an account?"),
+                    ButtonText(text: 'Sing up',nextScreen: RegistrationScreen(),)
+                  ],
+                )
+              ],
+            ),
           ),
         ),
       ),
--- lib/features/registration_screen/screens/registration_screen.dart
+++ lib/features/registration_screen/screens/registration_screen.dart
@@ -8,38 +8,40 @@ class RegistrationScreen extends StatelessWidget {
   @override
   Widget build(BuildContext context) {
     return Scaffold(
-      body: Padding(
-        padding: const EdgeInsets.fromLTRB(32, 24, 32, 0),
-        child: Center(
-          child: Column(
-            mainAxisAlignment: MainAxisAlignment.center,
-            children: [
-              Image.asset("assets/images/cafe.png"),
-              Text('Get’s things done\nwith TODO',
-                textAlign: TextAlign.center,
-                style: Theme.of(context).textTheme.headlineLarge,
-              ),
-              const SizedBox(height:12),
-              Text('Let’s help you meet up your tasks',
-              style: Theme.of(context).textTheme.headlineSmall,),
-              const SizedBox(height:16),
-              const RoundTextField(hint: 'Enter your full name'),
-              const SizedBox(height:16),
-              const RoundTextField(hint: 'Enter your email'),
-              const SizedBox(height:16),
-              const RoundTextField(hint: 'Enter your password'),
-              const SizedBox(height:16),
-              const RoundTextField(hint: 'Confirm password'),
-              const SizedBox(height:32),
-              const GetStartButton(text: 'Register'),
-              const SizedBox(height:16),
-              const Row(
-                mainAxisAlignment: MainAxisAlignment.center,
-                children: [
-                  Text('Already have an account?',),
-                  ButtonText(text: 'Sing in',nextScreen: AutorizationScreen(),),
-                ],)
-            ],
+      body: SingleChildScrollView( //добавляет прокрутку экрану
+        child: Padding(
+          padding: const EdgeInsets.fromLTRB(32, 48, 32, 0),
+          child: Center(
+            child: Column(
+              mainAxisAlignment: MainAxisAlignment.center,
+              children: [
+                Image.asset("assets/images/cafe.png"),
+                Text('Get’s things done\nwith TODO',
+                  textAlign: TextAlign.center,
+                  style: Theme.of(context).textTheme.headlineLarge,
+                ),
+                const SizedBox(height:12),
+                Text('Let’s help you meet up your tasks',
+                style: Theme.of(context).textTheme.headlineSmall,),
+                const SizedBox(height:16),
+                const RoundTextField(hint: 'Enter your full name'),
+                const SizedBox(height:16),
+                const RoundTextField(hint: 'Enter your email'),
+                const SizedBox(height:16),
+                const RoundTextField(hint: 'Enter your password'),
+                const SizedBox(height:16),
+                const RoundTextField(hint: 'Confirm password'),
+                const SizedBox(height:32),
+                const GetStartButton(text: 'Register'),
+                const SizedBox(height:16),
+                const Row(
+                  mainAxisAlignment: MainAxisAlignment.center,
+                  children: [
+                    Text('Already have an account?',),
+                    ButtonText(text: 'Sing in',nextScreen: AutorizationScreen(),),
+                  ],)
+              ],
+            ),
           ),
         ),
       )
